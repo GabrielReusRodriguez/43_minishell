bcc76f0a097d595ecb51c78f73c715d0ec5dd372
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-11-08 01:20:57 +0100

    Creada la config del shell con las copias de los fds stdin y stdout

--- include/config.h
+++ include/config.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:10:58 by gabriel           #+#    #+#             */
-/*   Updated: 2024/11/04 21:40:23 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/08 01:19:49 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 //# define  SHELL_DEFAULT_PROMPT "minishell> " 
 
 # define  SHELL_DEFAULT_PROMPT COLOUR_BLUE "minishell> " COLOUR_RST 
+
+typedef struct s_shell_config
+{
+	int fd_copy_stdin;
+	int fd_copy_stdout;
+} t_shell_config;
 
 #endif
--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 20:25:50 by gabriel           #+#    #+#             */
-/*   Updated: 2024/11/04 20:59:35 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/08 01:20:23 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdbool.h>
 # include <stdlib.h>
+
+# include "config.h" 
 
 typedef enum e_shell_mode
 {
@@ -28,7 +30,8 @@ typedef struct s_minishell
 	unsigned char	last_status;
 	char			*cmd;
 	t_shell_mode	mode;
-
+	t_shell_config	config;
+	
 }	t_minishell;
 
 bool	minishell_init(t_minishell *shell);
